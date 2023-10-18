b9b32058f10117d366dbf696e855de8def57b4c6
ArturLysovych <arturlisovic@gmail.com>
2023-10-18 22:10:55 +0300

    Maked Info-component animation with grows numbers
    Used hooks: useEffects, useState

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -1,25 +1,65 @@
-import React from 'react'
+import { React, useState, useEffect } from 'react'
 import './css/Info.css';
 
 export default function Info() {
+
+    const [freelancerCount, setFreelancerCount] = useState(0);
+    const [visitorsCount, setVisitorsCount] = useState(0);
+    const [contractCount, setContractCount] = useState(0);
+    const [companiesCount, setCompaniesCount] = useState(0);
+    const [funcCalled, setFuncCalled] = useState(false);
+  
+    useEffect(() => {
+      const containerTop = document.querySelector('.Info').getBoundingClientRect().top;
+      const setCount = (number, updateFunction, delay) => {
+        let thisNum = 0;
+        let addTimeOut;
+        const increment = () => {
+          thisNum += 100;
+          updateFunction(thisNum);
+          if (thisNum < number) addTimeOut = setTimeout(increment, delay);
+          else if (thisNum >= number) {
+            thisNum -= thisNum - number;
+            updateFunction(thisNum);
+            clearTimeout(addTimeOut);
+          }
+        };
+        increment();
+      };
+      const handleScroll = () => {
+        const scrollPos = window.scrollY;
+        if (scrollPos >= containerTop - 500 && !funcCalled) {
+          setCount(36789, setFreelancerCount, 2);
+          setCount(45973, setVisitorsCount, 1);
+          setCount(56461, setContractCount, 1);
+          setCount(852, setCompaniesCount, 150);
+          setFuncCalled(true);
+        }
+      };
+      window.addEventListener('scroll', handleScroll);
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, [funcCalled]);
+    
   return (
     <div className='Info'>
         <div className="container">
             <div className="info-container">
                 <h2>
-                    36,789
+                    {freelancerCount}
                     <span>Talented Freelancer</span>
                 </h2>
                 <h2>
-                    45,973
+                    {visitorsCount}
                     <span>Visitors</span>
                 </h2>
                 <h2>
-                    56,461
+                    {contractCount}
                     <span>Contract Made</span>
                 </h2>
                 <h2>
-                    852k
+                    {companiesCount}k
                     <span>Companies</span>
                 </h2>
             </div>
